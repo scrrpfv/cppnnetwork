b2691bc62fe50bc5d3e9ee7d2827e653d1870f13
scrrpfv <afp2@cin.ufpe.br>
2024-04-15 10:47:06 -0300

    Código alterado para não utilziar vetores Provavelmente ainda não funciona, e as funções precisam ser alteradas para void pra melhor clareza e para um melhor código

--- network.cpp
+++ network.cpp
@@ -1,68 +1,93 @@
 #include <iostream>
-#include <vector>
 #include <random>
-#include <algorithm>
 #include <iterator>
 #include <numeric>
 
 using namespace std;
 
-/* Function to initialize network's columns with random values */
-vector<float> GenerateColumn(int n)
+/*
+TO DO:
+Trocar todas as funções por void eu acho?
+*/
+
+struct NETWORK
+{
+    float **weights; /* Set of matrices defining the network's weights */
+    float **biases; /* Set of arrays defining the network's biases */
+    int *acti_funcs;
+    int *sizes;
+    int n_layers;
+};
+
+float **GenerateBiases(int *sizes)
+{
+    int n_layers = sizeof(sizes) / sizeof(sizes[0]) - 1;
+    float **biases;
+    biases = (float **) malloc (sizeof(float*) * n_layers);
+    for(int l=0; l<n_layers; l++)
+    {
+        int n = sizes[l], m = sizes[l+1];
+        float layer[n * m];
+        biases[l] = layer;
+    }
+    return biases;
+}
+
+/* Function to initialize network's layers with random values */
+float *GenerateLayer(int n, int m)
 {
     /* Creating the rng with normal distribution around 0 */
     random_device rd;
     mt19937_64 gen(rd());
     normal_distribution<float> dis(0, 1);
-    auto rand = [&](){ return dis(gen); }; /* Ugly piece of code to ensure no repeats */
-
-    vector<float> column(n);
+    auto rand = [&](){ return dis(gen); }; /* Different function call for each value to ensure no repeats */
+    float layer[n * m]; /* Sadly using a 1d array to avoid triple pointer madness */
+    float (*ptr)[m];
     for(int i=0; i<n; i++)
     {
-        column[i] = rand(); /* Attributes random values to the columns */
+        for(int j=0; j<m; j++)
+        {
+            layer[i * m + j] = rand(); /* Attributes random values to the layers */
+        }
     }
-    return column;
+    return layer;
 }
 
-/* Function to initialize the weights of a network using the GenerateColumn function iteratively */
-vector<vector<float>> GenerateWeights(vector<int> sizes)
+/* Function to initialize the weights of a network using the GenerateLayer function iteratively */
+float **GenerateWeights(int* sizes)
 {
-    vector<vector<float>> weights;
-    for(int rowsize : sizes)
+    int n_layers = sizeof(sizes) / sizeof(sizes[0]) - 1;
+    float **weights;
+    weights = (float**) malloc (sizeof(float*) * n_layers);
+    for(int i = 0; i<n_layers; i++)
     {
-        weights.push_back(GenerateColumn(rowsize)); /* Adds new column to the weights vector */
+        int n = sizes[i], m = sizes[i+1]; 
+        float *layer = GenerateLayer(n, m);
+        weights[i] = layer; /* Adds new layers to the weights array */
     }
     return weights;
 }
 
+NETWORK InitNetwork(int *sizes, int *acti_funcs)
+{
+    NETWORK network;
+    network.weights = GenerateWeights(sizes);
+    network.biases = GenerateBiases(sizes);
+    network.acti_funcs = acti_funcs;
+    network.sizes = sizes;
+    network.n_layers = sizeof(sizes) / sizeof(sizes[0]) - 1;
+    return network;
+}
+
 /* Function to define the feed forward loop for the neural network based on the entry data and activation functions passed*/
-vector<float> FeedForward(
-    vector<vector<float>> weights,
-    vector<vector<float>> biases, /* TODO add function to initialize biases (maybe all set to 0) */
-    vector<float> entry_data,
-    vector<int> activation_functions /* TODO define some differente ativation functions for the ff loop*/
-    )
+float *FeedForward(NETWORK network, float *entry_data)
     /* ADD actual code for the feed forward loop */
 {
-    int n_columns = weights.size();
-    vector<float> result(n_columns); /* Random placeholder */
-    return result;
+    float values[2] = {10, 20};
+    return values;
 }
 
 int main()
 {
-    vector<int> size = {5, 10, 10, 5};
-    vector<vector<float>> weights = GenerateWeights(size);
-    for(int i=0; i<weights.size(); i++)
-    {
-        cout << "Column: " << i << endl;
-
-        vector<float> column = weights[i];
-
-        for(int j=0; j<column.size(); j++)
-        {
-            cout << column[j] << endl;
-        }
-    }
     return 0;
 }
